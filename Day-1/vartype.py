783b0fd48be88b2fd9dfaeec35eb25bb3efe4e63
LokeshVasa <lokeshchandra842@gmail.com>
2024-10-25 22:17:19 +0530

    y

--- Day-1/vartype.py
+++ Day-1/vartype.py
@@ -1,1 +1,15 @@
-print(f"{(4/3)*3.14*(float(input())**3):.2f}" )
+# print(f"{(4/3)*3.14*(float(input())**3):.2f}" )
+
+
+import pyautogui
+import time
+
+# Give yourself a few seconds to switch to the target application
+time.sleep(5)
+
+# The text you want to type
+
+for i in range(10):
+    text_to_type = '''nenu oka pedha yedhava'''
+    pyautogui.typewrite(text_to_type, interval=0.1) 
+    pyautogui.press('enter')
