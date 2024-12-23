6950932d0a50bf2d09bf3b9689df048ee434ba5c
LokeshVasa <lokeshchandra842@gmail.com>
2024-12-23 21:47:50 +0530

    commit

--- Day-1/1-10loop.py
+++ Day-1/1-10loop.py
@@ -1,2 +1,3 @@
-for i in range(1,11):
-    print(i)               
+l="lokesh"
+c="chandra"
+print(f"l= {l}+c= {c}.")
---
+++ Day-1/input.py
@@ -0,0 +1,4 @@
+start=int(input("Enter how time do you want to print"))
+for i in range(start):
+    name=input("enter a name:")
+    print(name)     
