706f9b623a84832635c7778f772b27dbc7340542
LokeshVasa <lokeshchandra842@gmail.com>
2025-02-09 21:33:55 +0530

    push

---
+++ area/areacal.py
@@ -0,0 +1,6 @@
+print(" 1.square kilometre \n 2. square metre \n 3.square mile \n 4. square yard \n 5. Square foot \n 6. Square inch \n 7. hectare ")
+inputt=int(input("enter a to which you want to convert:-"))
+print(" 1.square kilometre \n 2. square metre \n 3.square mile \n 4. square yard \n 5. Square foot \n 6. Square inch \n 7. hectare ")
+inputt2=int(input("enter to which you want to convert:-"))
+if (inputt == inputt2):
+    
