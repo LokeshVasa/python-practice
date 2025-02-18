288c2477813ee1b5264a09860774a7aafa3f64e3
LokeshVasa <lokeshchandra842@gmail.com>
2025-02-18 06:56:45 +0530

    17-02

---
+++ PythonStringExercise/findingsubstring.py
@@ -0,0 +1,13 @@
+'''Find the last position of a given substring
+Write a program to find the last position of a substring “Emma” in a given string.
+
+Given:
+
+str1 = "Emma is a data scientist who knows Python. Emma works at google."
+Expected Output:
+
+Last occurrence of Emma starts at index 43'''
+
+str1 = "Emma is a data scientist who knows Python. Emma works at google."
+x=str1.rfind("Emma")
+print(x)
