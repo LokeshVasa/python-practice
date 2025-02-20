2163475d4be69d2b4ee4f4cdf4b8992b94e622d1
LokeshVasa <lokeshchandra842@gmail.com>
2025-02-20 21:32:23 +0530

    20-02-2025

--- PythonStringExercise/stringbalance.py
+++ PythonStringExercise/stringbalance.py
@@ -1,6 +1,7 @@
 '''
 String characters balance Test
-Write a program to check if two strings are balanced. For example, strings s1 and s2 are balanced if all the characters in the s1 are present in s2. The character’s position doesn’t matter.
+Write a program to check if two strings are balanced. For example, strings s1 and s2 are
+balanced if all the characters in the s1 are present in s2. The character’s position doesn’t matter.
 
 Given:
 
@@ -21,3 +22,4 @@ False
 
 
 '''
+
