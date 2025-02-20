bcffebff5318695d65392f5bfaefbab4761c80b1
LokeshVasa <lokeshchandra842@gmail.com>
2025-02-20 21:32:11 +0530

    20-02-2025

--- PythonStringExercise/largestwordingivenstring.py
+++ PythonStringExercise/largestwordingivenstring.py
@@ -3,7 +3,14 @@ string1='used as an informal greeting, usually to people who you know'
 
 '''
 string1='used as an informal greeting, usually to people who you know'
-empty_list=[]
-for i in string1:
-    empty_list.append(i)
-    print(max(empty_list))    
+words=string1.split(' ')
+#print(words)   
+maxx=0
+max_word=''
+for word in words:
+    #print(word,len(word))
+    length=len(word)
+    if length>maxx:
+        max_word=word
+        maxx=length
+print(max_word)
