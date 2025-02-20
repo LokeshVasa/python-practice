4c9dd62381bb9d024fd470ab84b90dbb7d435f77
LokeshVasa <lokeshchandra842@gmail.com>
2025-02-20 09:02:17 +0530

    20-02-2025

---
+++ PythonStringExercise/largestwordingivenstring.py
@@ -0,0 +1,9 @@
+'''
+string1='used as an informal greeting, usually to people who you know'
+
+'''
+string1='used as an informal greeting, usually to people who you know'
+empty_list=[]
+for i in string1:
+    empty_list.append(i)
+    print(max(empty_list))    
