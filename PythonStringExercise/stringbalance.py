c3d9d9958d8b50d9eca9ffc81947e3697c867c54
LokeshVasa <lokeshchandra842@gmail.com>
2025-02-21 08:48:29 +0530

    21-02-2025

--- PythonStringExercise/stringbalance.py
+++ PythonStringExercise/stringbalance.py
@@ -4,22 +4,26 @@ Write a program to check if two strings are balanced. For example, strings s1 an
 balanced if all the characters in the s1 are present in s2. The character’s position doesn’t matter.
 
 Given:
-
 Case 1:
-
 s1 = "Yn"
 s2 = "PYnative"
 Expected Output:
-
 True
 Case 2:
-
 s1 = "Ynf"
 s2 = "PYnative"
 Expected Output:
-
 False
-
-
 '''
+s1= "Ynk"
+s2 ="PYnative"
+count=0
+for i in s1:
+    if i in s2:
+        count+=1
+        print(i)
+if count == len(s1):
+    print("all characters are there")
+else:
+    print("try again")        
 
