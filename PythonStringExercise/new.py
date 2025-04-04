fd4562ddbe1fa4fb17f8bd71ea023cdd0560e67e
LokeshVasa <lokeshchandra842@gmail.com>
2025-04-04 13:03:27 +0530

    04-04-2025

--- PythonStringExercise/new.py
+++ PythonStringExercise/new.py
@@ -1,5 +1,12 @@
-l=list(input("enter message:-"))
-if not l:
-    print("True")
+'''l=list(input("enter message:-"))
+s=len(l)
+if s==0:
+    print("empty")
 else:
-    print("False")
+    print("not empty")    
+    '''
+l=list(input("enter message:-"))
+if l[0]==None:
+    print("true")
+else:
+    print("False")        
