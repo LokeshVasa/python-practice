0c91284f6befcddf037b836dfaa8f54e92c0227b
LokeshVasa <lokeshchandra842@gmail.com>
2025-04-09 22:07:09 +0530

    09-04-2025

--- Day-12/generate_qr.py
+++ Day-12/generate_qr.py
@@ -3,3 +3,12 @@ import qrcode
 qr = qrcode.make("Hello, This is lokesh!")
 qr.save("myqr.png")
 print("QR code saved as myqr.png")
+
+#----------------------------------------------
+
+
+import qrcode
+
+qr = qrcode.make("Hello, This is lokesh!")
+qr.save("myqr.png")
+print("QR code saved as myqr.png")
